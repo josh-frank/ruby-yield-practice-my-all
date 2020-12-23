e37e87ae81d54ebb69ec2cf5e8c010f0231aef5d
Josh Frank <eurosewusa@gmail.com>
2020-12-22 19:43:31 -0500

    lobster thermidor

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,12 @@
 require 'pry'
 
 def my_all?(collection)
-
+    i = 0
+    result = []
+    while i < collection.length
+        result << yield( collection[ i ] )
+        i += 1
+    end
+    return false if result.include?( false )
+    true
 end
